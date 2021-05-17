dc9f1b133554cb340bc3ebdbcfc46651e9f05d7d
DESKTOP-RKMKLHA\owner <2825873215@QQ.COM>
2021-05-17 18:23:31 +0800

    add temporary queues example

---
+++ temporay/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/miaogaolin/rabbitmq-example/utils"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	utils.FailOnError(err, "Failed to connect to RabbitMQ")
+	defer func() {
+		time.Sleep(time.Minute * 30)
+		conn.Close()
+	}()
+
+	ch, err := conn.Channel()
+	utils.FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+
+	// 消费
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		true,   // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	utils.FailOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	// 发布消息
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "text/plain",
+			Body:         []byte(body),
+		})
+	utils.FailOnError(err, "Failed to publish a message")
+	<-forever
+}
