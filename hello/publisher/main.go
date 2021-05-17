7fd0cc79b942a02cf4f2f2806e6267f03380e388
DESKTOP-RKMKLHA\owner <2825873215@QQ.COM>
2021-05-17 14:56:57 +0800

    add hello world example

---
+++ hello/publisher/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/miaogaolin/rabbitmq-example/utils"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	utils.FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	utils.FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	utils.FailOnError(err, "Failed to publish a message")
+}
