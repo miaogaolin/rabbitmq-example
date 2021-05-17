4cca307b42f0efc19a88ee698a51a8d5354061a4
DESKTOP-RKMKLHA\owner <2825873215@QQ.COM>
2021-05-17 16:17:45 +0800

    add work queue

---
+++ workqueus/consumer/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"time"
+
+	"github.com/miaogaolin/rabbitmq-example/utils"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	utils.FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	utils.FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	utils.FailOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			dotCount := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dotCount)
+			time.Sleep(t * time.Second)
+			log.Printf("Done")
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
--- workqueus/publisher/main.go
+++ workqueus/publisher/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+	"os"
+	"strings"
+
 	"github.com/miaogaolin/rabbitmq-example/utils"
 	"github.com/streadway/amqp"
 )
@@ -22,16 +26,28 @@ func main() {
 		false,   // no-wait
 		nil,     // arguments
 	)
-	body := "Hello World!"
+	body := bodyFrom(os.Args)
 	err = ch.Publish(
 		"",     // exchange
 		q.Name, // routing key
 		false,  // mandatory
-		false,  // immediate
+		false,
 		amqp.Publishing{
 			DeliveryMode: amqp.Persistent,
 			ContentType:  "text/plain",
 			Body:         []byte(body),
 		})
 	utils.FailOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s", body)
+
+}
+
+func bodyFrom(args []string) string {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "hello"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	return s
 }
