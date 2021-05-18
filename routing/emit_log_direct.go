4a04c8697af89276e175d0e5c92fde6dffb10e24
DESKTOP-RKMKLHA\owner <2825873215@QQ.COM>
2021-05-18 14:48:52 +0800

    add direct exchange  example

---
+++ routing/emit_log_direct.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/miaogaolin/rabbitmq-example/utils"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	utils.FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	utils.FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"logs_direct", // name
+		"direct",      // type
+		true,          // durable
+		false,         // auto-deleted
+		false,         // internal
+		false,         // no-wait
+		nil,           // arguments
+	)
+	utils.FailOnError(err, "Failed to declare an exchange")
+
+	body := utils.BodyFrom(os.Args)
+	err = ch.Publish(
+		"logs_direct",         // exchange
+		severityFrom(os.Args), // routing key
+		false,                 // mandatory
+		false,                 // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	utils.FailOnError(err, "Failed to publish a message")
+
+	log.Printf(" [x] Sent %s", body)
+}
+
+func severityFrom(args []string) string {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "info"
+	} else {
+		s = os.Args[1]
+	}
+	return s
+}
